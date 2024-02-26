15806d900b8d73aaf709822df5f78cf58f908f06
PAVAN KRISHNA A G <pavanu26122003@gmail.com>
2024-02-27 02:53:05 +0530

    Project completed

---
+++ script.js
@@ -0,0 +1,11 @@
+let calculation = localStorage.getItem("save") || "";
+display();
+function update(value) {
+	calculation += value;
+	console.log(calculation);
+	display();
+	localStorage.setItem("save", calculation);
+}
+function display() {
+	document.querySelector(".text").innerHTML = `${calculation}`;
+}
